65859d19232d753a03ab5ac654100307c2635ddf
k.madhavv123 <k.madhavv123@gmail.com>
2024-07-08 19:00:07 +0000

    "major"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Login from './modules/common/Login'
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import Header from './components/Header'
 import Register from './modules/common/Register'
+import AllProperties from './modules/user/Renter/AllProperties'
 function App() {
 
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path='/home' element={<Home/>}/>
           <Route path='/login' element={<Login/>}/>
           <Route path='/register' element={<Register/>}/>
+          <Route path='/prp' element={<AllProperties/>}/>
         </Routes>
       </Router>
     </>
---
+++ frontend/src/modules/user/Renter/AllProperties.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Cards from '../../../components/Cards'
+
+function AllProperties() {
+    return (
+
+        <>
+            <div>
+                <main class="py-8 px-4 md:px-6 mt-8">
+                    <div class="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+
+                        <Cards/>
+                        <Cards/>
+                        <Cards/>
+                        <Cards/>
+
+
+                    </div>
+                </main>
+            </div>
+        </>
+    )
+}
+
+export default AllProperties
