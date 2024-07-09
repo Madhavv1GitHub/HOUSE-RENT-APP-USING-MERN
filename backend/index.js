b77e852055f1b6454ef6c587edf1952b4f07ec30
k.madhavv123 <k.madhavv123@gmail.com>
2024-07-09 09:06:54 +0000

    "db update"

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app=express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb://127.0.0.1:27017/houserent");
+mongoose.connect("mongodb://localhost:27017/houserent");
 
 app.post('/r',(req,res)=>{
     userModel.create(req.body)
